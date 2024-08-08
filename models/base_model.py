0e717f3cbe055090284b0fe16f3a579c98078e2b
Peter ALX <quaypeter25@gmail.com>
2024-08-08 12:26:43 +0000

    change base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,44 @@
 #!/usr/bin/python3
-"""
-The base class for all hbnb models is defined here
-"""
-from datetime import datetime, timedelta
-from uuid import uuid4
+import uuid
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """
-    The base for all hbnb models
-    all hbnb models extend this class
-      Attributes:
-        id (uuid string): The model id.
-        created_at (datetime): The datetime at creation.
-        updated_at (datetime): The datetime of last update.
-    """
-    def __init__(self):
-        """ init the base model """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *ar, **kw):
+        if kw:
+            for k, v in kw.items():
+                # potential oversight: what if key is int
+                # potential oversight: test for a dict without the expected
+                #   keys/variables?
+                # potential oversight: test for a dict without expected
+                # keys/variables?
+                # should add these to tests?
+                if k in ['created_at', 'updated_at']:
+                    # 👆what if other attrs are dates?
+                    setattr(self, k, datetime.strptime(
+                        v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k != '__class__':
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
-        """ Return a string representation of the BaseModel instance """
-        return (f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        """
-        persists changes made to a BaseModel instance in future
-        and reflects the time of update
-        -> persisting not implemented yet
-        """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """
-        returns a json serealizable dictionary representation
-        of an instance
-        """
         d = {}
-        d['__class__'] = self.__class__.__name__
         for k, v in self.__dict__.items():
-            if type(v) is datetime:
+            if isinstance(v, datetime):
                 d[k] = v.isoformat()
             else:
                 d[k] = v
+        d['__class__'] = self.__class__.__name__
         return (d)

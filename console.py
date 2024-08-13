6205177ce846ab15dbee482677c482a033fc859d
Peter ALX <quaypeter25@gmail.com>
2024-08-13 07:23:42 +0000

    start writing the console

--- console.py
+++ console.py
@@ -2,9 +2,23 @@
 
 from models.base_model import BaseModel
 from models import storage
+import cmd
 
-print(storage.all())
-tst = BaseModel()
-tst.deez = 'nutz'
-print(storage.all())
-tst.save()
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """quit command exits the program"""
+        return True
+
+    def do_EOF(self, args):
+        print()
+        return True
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
